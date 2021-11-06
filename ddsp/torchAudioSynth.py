f8d9d08b9451dc6daaef67746859a52a78873fbe
Azerty-Labs <75258525+azerty-waves@users.noreply.github.com>
2021-11-06 16:03:19 +0100

    harmonic_synth from acids

---
+++ ddsp/torchAudioSynth.py
@@ -0,0 +1,10 @@
+import torch
+import math
+
+def harmonic_synth(pitch, amplitudes, sampling_rate):
+    n_harmonic = amplitudes.shape[-1]
+    omega = torch.cumsum(2 * math.pi * pitch / sampling_rate, 1)
+    omegas = omega * torch.arange(1, n_harmonic + 1).to(omega)
+    signal = (torch.sin(omegas) * amplitudes).sum(-1, keepdim=True)
+    return signal
+
